65b62c58376cd2d88a2b2f9daad1ff92f2e16dbc
Noman Dhoni <92979541+nomandhoni-cs@users.noreply.github.com>
2022-11-03 12:52:40 +0000

    Added course data as an objcet

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import { Body } from './components/Body/Body';
 import { Header } from './components/Header/Header';
 import logo from './logo.svg'
 import bannerImage from './images/topGreenBanner.webp'
-
+const courseInfo = [
+  {title: 'React crash course 2022',
+  price: 19.99,
+  insturctor: 'Noman Dhoni',
+  length: '4h 40min'    
+},
+  {title: 'React crash course 2022',
+  price: 19.99,
+  insturctor: 'Noman Dhoni',
+  length: '4h 40min'    
+},
+  {title: 'React crash course 2022',
+  price: 19.99,
+  insturctor: 'Noman Dhoni',
+  length: '4h 40min'    
+},
+  {title: 'React crash course 2022',
+  price: 19.99,
+  insturctor: 'Noman Dhoni',
+  length: '4h 40min'    
+}
+]
 function App() {
   return (
     <div className="App">
       <Header logo={logo}/>
-      <Body halfBanner={bannerImage}/>
+      <Body courseList={courseInfo} halfBanner={bannerImage}/>
     </div>
   );
 }
