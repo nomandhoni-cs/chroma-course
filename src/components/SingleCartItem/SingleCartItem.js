fafbd1beda474b3d53f91d2111fd0f4a72d2f7bc
Noman Dhoni <92979541+nomandhoni-cs@users.noreply.github.com>
2023-08-07 09:30:08 +0000

    Design of cart done

--- src/components/SingleCartItem/SingleCartItem.js
+++ src/components/SingleCartItem/SingleCartItem.js
@@ -1,29 +1,36 @@
-// import { Col } from "react-bootstrap";
 import "./SingleCartItem.css";
+import React from 'react';
+import { BsTrash } from 'react-icons/bs';
+
 const SingleCartItem = (props) => {
-    const { title, instructor, price, course_thumbnail } = props.singleItem;
-    console.log(props)
+    const { course_thumbnail, title, instructor, price } = props.singleItem;
+
     return (
-        <>
-            <div className="single-cart-item-container">
-                <div className="image-and-title-container">
-                    <div className="course-image">
-                        <img src={course_thumbnail} alt={title} />
-                    </div>
-                    <div className="course-title-instructor">
-                        <div className="title">{title}</div>
-                        <div className="instructor">{instructor}</div>
-                    </div>
+        <div className="single-cart-item-container">
+            <div className="image-and-title-container">
+                <div className="course-image">
+                    <img src={course_thumbnail} alt={title} />
                 </div>
-                <div className="price-and-remove-container">
-                    <div className="course-price"><span>Price: {price}</span></div>
-                    <div className="course-remove">
-                        <button onClick={() => props.removeItem(props.singleItem)}>Remove</button>
-                    </div>
+                <div className="course-title-instructor">
+                    <div className="title">{title}</div>
+                    <div className="instructor">by {instructor}</div>
                 </div>
             </div>
-        </>
-    )
-}
+            <div className="price-and-remove-container">
+                <div className="course-price">
+                    <span id="price-text">Price:$ {price}</span>
+                </div>
+                <div className="course-remove">
+                    <button onClick={() => props.removeItem(props.singleItem)}>
+                        <BsTrash />
+                        <span id="remove-btn-text">
+                            Remove
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default SingleCartItem
+export default SingleCartItem;
