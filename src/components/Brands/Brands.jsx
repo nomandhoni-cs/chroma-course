dfd354d249857e03e4a0ff6c6f05785f9137b10c
nomandhoni-cs <nomandhoni@yahoo.com>
2024-02-20 11:52:31 +0600

    Added Brands logo

---
+++ src/components/Brands/Brands.jsx
@@ -0,0 +1,50 @@
+const Brands = () => {
+  return (
+    <div className="py-24 sm:py-32">
+      <div className="mx-auto container px-6 lg:px-8">
+        <h2 className="text-center text-lg font-semibold leading-8 text-light-primary dark:text-dark-primary">
+          Trusted by the world’s most innovative teams
+        </h2>
+        <div className="mx-auto container p-5 md:p-15 lg:p-20 rounded-lg bg-light-tertiary dark:bg-white mt-10 grid grid-cols-4 items-center gap-x-8 gap-y-10  sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+            alt="Transistor"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+            alt="Reform"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+            alt="Tuple"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+            alt="SavvyCal"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+            alt="Statamic"
+            width={158}
+            height={48}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Brands;
