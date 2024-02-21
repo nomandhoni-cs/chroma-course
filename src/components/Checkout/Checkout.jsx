3148ee19447fa5bff2c756ddec82578033000387
nomandhoni-cs <nomandhoni@yahoo.com>
2024-02-21 17:06:20 +0600

    Changed the whole cart layout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -4,7 +4,7 @@ import { Cart } from "../Cart/Cart";
 const Checkout = ({ cart, removeFromCart, unitPrice }) => {
   console.log("Cart", cart);
   return (
-    <div className="container mx-auto">
+    <div className="cart container ">
       <Cart cart={cart} removeFromCart={removeFromCart} unitPrice={unitPrice} />
     </div>
   );
--- src/components/SingleCartItem/SingleCartItem.jsx
+++ src/components/SingleCartItem/SingleCartItem.jsx
@@ -1,10 +1,10 @@
-import "./SingleCartItem.css";
 import React from "react";
-import { BsTrash } from "react-icons/bs";
 import Swal from "sweetalert2";
+import { HiTrash } from "react-icons/hi";
 
 const SingleCartItem = (props) => {
   const { course_thumbnail, title, instructor, price } = props.singleItem;
+
   // Function to handle item removal
   const handleRemove = () => {
     Swal.fire({
@@ -27,27 +27,35 @@ const SingleCartItem = (props) => {
       }
     });
   };
+
   return (
-    <div className="single-cart-item-container">
-      <div className="image-and-title-container">
-        <div className="course-image">
-          <img src={course_thumbnail} alt={title} />
+    <div className="bg-white dark:bg-dark-secondary shadow rounded-lg p-4 sm:p-6 flex flex-col sm:flex-row justify-between items-center mb-4">
+      <div className="flex items-center mb-4 sm:mb-0">
+        <div className="w-24 h-24 sm:w-20 sm:h-20 bg-gray-100 rounded-lg overflow-hidden mr-4">
+          <img
+            src={course_thumbnail}
+            alt={title}
+            className="w-full h-full object-cover"
+          />
         </div>
-        <div className="course-title-instructor">
-          <div className="title">{title}</div>
-          <div className="instructor">by {instructor}</div>
+        <div>
+          <h2 className="text-base sm:text-lg font-semibold text-black dark:text-gray-200">
+            {title}
+          </h2>
+          <p className="text-sm text-gray-500">by {instructor}</p>
         </div>
       </div>
-      <div className="price-and-remove-container">
-        <div className="course-price">
-          <span id="price-text">Price:$ {price}</span>
-        </div>
-        <div className="course-remove">
-          <button onClick={handleRemove}>
-            <BsTrash />
-            <span id="remove-btn-text">Remove</span>
-          </button>
-        </div>
+      <div className="flex items-center justify-between sm:justify-end w-full sm:w-auto mt-4 sm:mt-0">
+        <p className="text-base sm:text-lg font-semibold text-black dark:text-gray-200 mr-2 sm:mr-4">
+          ${price}
+        </p>
+        <button
+          className="font-body font-semibold text-red-500 mt-2 flex items-center bg-red-100 hover:bg-red-200 dark:text-black rounded-md px-3 py-1"
+          onClick={handleRemove}
+        >
+          <HiTrash className="mr-1" />
+          <span className="hidden sm:inline">Remove</span>
+        </button>
       </div>
     </div>
   );
